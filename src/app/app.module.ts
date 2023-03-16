ec7317ca0e146daa2c37bdf17ee6247f2591583a
lucas h <lucas.ph@live.com>
2023-03-16 17:38:08 -0300

    first-challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { MainComponent } from './components/main/main.component';
+import { PageWraperComponent } from './components/page-wraper/page-wraper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AsideComponent,
+    MainComponent,
+    PageWraperComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/page-wraper/page-wraper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-wraper',
+  templateUrl: './page-wraper.component.html',
+  styleUrls: ['./page-wraper.component.css']
+})
+export class PageWraperComponent {
+
+}
